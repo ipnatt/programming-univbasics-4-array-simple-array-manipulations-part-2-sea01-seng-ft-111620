3ba7d68e76a8eae2606548429c71b13eeb94a2c6
Irvin Natt <ipnatt@gmail.com>
2020-11-05 11:47:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,9 +2,7 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert
-  array.insert(3, element)
-  array[3] = element
+def using_insert(array, element)
 end
 
 def using_uniq(array)
